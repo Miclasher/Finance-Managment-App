8c267eba15e201f9ef47fabdf828540978fcd652
Miclasher <14122006mg@gmail.com>
2025-02-16 22:22:23 +0200

    Add required DTOs

---
+++ Shared/FinancialOperationDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public sealed class FinancialOperationDTO
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [MaxLength(1000)]
+        public string? UserComment { get; set; }
+        [Required]
+        public Guid TransactionTypeId { get; set; }
+        public TransactionTypeDTO TransactionType { get; set; } = null!;
+    }
+}
---
+++ Shared/FinancialOperationForCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public sealed class FinancialOperationForCreateDTO
+    {
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [MaxLength(1000)]
+        public string? UserComment { get; set; }
+        [Required]
+        public Guid TransactionTypeId { get; set; }
+    }
+}
